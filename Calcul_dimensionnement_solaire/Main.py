35836f428efd7070a57711c4639cc533ca03f7c7
Théo Delpech <86845950+BelPeche@users.noreply.github.com>
2021-07-26 00:32:35 +0200

    Tente=ative debug

--- Calcul_dimensionnement_solaire/Main.py
+++ Calcul_dimensionnement_solaire/Main.py
@@ -1,3 +1,9 @@
-import Potentiel_Localisation
-potentiel_test = Potentiel_Localisation()
-print (potentiel_test.Potentiel_solaire_theo())
+from Potentiel_Localisation import Potentiel_Loc
+import solar_mod as sm
+Longitude = 47.22
+Latitude = -1.55
+Theo_ou_meteo = True
+beta = 10
+Albedo = 0.2
+potentiel_test = Potentiel_Loc(Longitude,Latitude,Theo_ou_meteo,beta,Albedo)
+print(potentiel_test.Potentiel_solaire_theo())
