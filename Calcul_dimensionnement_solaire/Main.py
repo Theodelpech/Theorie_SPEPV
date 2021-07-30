c9bf71a583c893deba80348343dd17c20635d725
Théo Delpech <86845950+BelPeche@users.noreply.github.com>
2021-07-30 09:59:34 +0200

    Version calcul théorique marche

--- Calcul_dimensionnement_solaire/Main.py
+++ Calcul_dimensionnement_solaire/Main.py
@@ -5,13 +5,16 @@ import numpy as np
 import csv
 from Input import input
 Title = "Saisie des coordonnées de la localisation"
-demande = "Quelle est la longitude ?"
-Longitude = input(Title, demande)
-Latitude = 47.22
+demandeLong = "Quelle est la longitude ?"
+Longitude = input(Title, demandeLong)
+demandeLat = "Quelle est la latitude ?"
+Latitude = input(Title, demandeLat)
 Theo_ou_meteo = True
 beta = 10
 Albedo = 0.2
-potentiel_test = Potentiel_Localisation(Longitude.message(),Latitude,Theo_ou_meteo,beta,Albedo)
+Lat = Latitude.message()
+Long = Longitude.message()
+potentiel_test = Potentiel_Localisation(Long.get(),Lat.get(),Theo_ou_meteo,beta,Albedo)
 Val_TestIt, Val_TestRb, Val_TestIo, Val_TestIth, Val_TestThe = potentiel_test.Potentiel_solaire_theo()
 print(max(Val_TestIt))
 y_valuesIt, y_valuesRb, y_valuesIo, y_valuesIth, y_valuesThe = potentiel_test.Potentiel_solaire_theo()
